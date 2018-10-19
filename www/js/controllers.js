185ec377f630dcf7f34bfca9f0bf2941f5c13bd6
ranjithrp <runjiet@gmail.com>
2018-10-19 16:07:06 +0530

    Initial Commit

---
+++ www/js/controllers.js
@@ -0,0 +1,14 @@
+angular.module('app.controllers', [])
+  
+.controller('homeCtrl', function($scope) {
+
+})
+   
+.controller('parkingSpacesCtrl', function($scope) {
+
+})
+   
+.controller('cloudCtrl', function($scope) {
+
+})
+    
